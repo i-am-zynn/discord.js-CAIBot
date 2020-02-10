7005b853ec2a4603ddd5d4875e858b30001e7950
NexusDeveloppement <59765200+NexusDeveloppement@users.noreply.github.com>
2020-02-10 15:16:14 +0100

    Add files via upload

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
     client.user.setPresence({
         game: {
-            name: "la version 1.0.0",
+            name: "la version 1.4.1",
             type: "WATCHING"
         }
     });
