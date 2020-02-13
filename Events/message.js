6de75ba62287237caa6bcf1a376c8039464ce2e8
NexusDeveloppement <59765200+NexusDeveloppement@users.noreply.github.com>
2020-02-13 16:16:03 +0100

    Add files via upload

--- Events/message.js
+++ Events/message.js
@@ -13,10 +13,6 @@ module.exports = (client, message) => {
     if (!message.content.startsWith(prefix)) {
         return;
     }
-
-    if (message.author.id != '581748249166217232' && message.author.id != '360010766876672000' && message.author.id != '541319683945791534' && message.author.id != '439333004247302145' && message.author.id != '479311894721724445' && message.author.id != '499608622003781632' && message.author.id != '504230013994926080' && message.author.id != '279205522970902528' && message.author.id != '83928726282174464' && message.author.id != '183602738435719168' && message.author.id != '529015301979897879') {
-        return message.channel.send('Vous n\'avez pas la permission de m\'utiliser');
-    }
     
         let args = message.content.slice(prefix.length).trim().split(/ +/g);
         let commande = args.shift();
--- Events/ready.js
+++ Events/ready.js
@@ -1,8 +1,20 @@
+const config = require('../config');
+
 module.exports = (client) => {
+
     client.user.setPresence({
         game: {
-            name: "la version 1.2.0",
+            name: `la version ${config.version}`,
             type: "WATCHING"
         }
     });
 };
+
+// module.exports = (client) => {
+//     client.user.setPresence({
+//         game: {
+//             name: "la version 1.2.0",
+//             type: "WATCHING"
+//         }
+//     });
+// };
