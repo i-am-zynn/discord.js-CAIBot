869710fc40b091767dd3f29682876f1925f561be
NexusDeveloppement <59765200+NexusDeveloppement@users.noreply.github.com>
2020-02-14 14:08:38 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -9,12 +9,3 @@ module.exports = (client) => {
         }
     });
 };
-
-// module.exports = (client) => {
-//     client.user.setPresence({
-//         game: {
-//             name: "la version 1.2.0",
-//             type: "WATCHING"
-//         }
-//     });
-// };
