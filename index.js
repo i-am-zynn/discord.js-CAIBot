ae9866280a47c1580305295bda51ce6d7e2fd2cf
NexusDeveloppement <59765200+NexusDeveloppement@users.noreply.github.com>
2020-02-14 14:06:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ client.commands = new Discord.Collection();
 const fs = require('fs');
 const config = require('./config');
 
-client.on('message', function (message) {
-    if (message.content.startsWith('Recherche')) {
-        let args = message.content.split(' ');
-        args.shift();
-
-        message.channel.send(`Voici le lien de redirection vers les résultats concernant votre recherche : https://www.google.fr/#q=${args.join('+')} \n \nVotre recherche : "${args.join(' ')}"`)
-            .then(console.log(`${message.author.username} a utilisé la commande Google pour rechercher : "${args.join(' ')}"`))
-            .catch((error) => {
-                console.error(error);
-
-                return message.channel.send('Une erreur s\'est produite lors de l\'exécution de la commande. Veuillez réessayer ultérieurement. Si le problème persiste, veuillez contacter Nεξυς#9063.')
-            })
-            
-    }
-})
-
 fs.readdir('./Commandes/', (error, f) => {
     if (error) { return console.error(error); }
         let commandes = f.filter(f => f.split('.').pop() === 'js');
